2f7ac13c0a508fc897e451986547947cdf7b284b
Aaron Guist <rhon.guist@gmail.com>
2023-08-07 17:52:17 -0700

    Adding initial file for deployment

--- devOpsWorkFlow.groovy
+++ devOpsWorkFlow.groovy
@@ -5,7 +5,7 @@ def sayHello() {
 
 def sourceCodeCheckout(sourceCodeUrl) {
     if ( sourceCodeUrl =~ /.git/ ) {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master'],[$class: 'WipeWorkspace'], [$class: 'UserExclusion', excludedUsers: 'amguist']], gitTool: 'Git 1.9.4', submoduleConfig: [], userRemoteConfigs: [[credentialsId: 'github_user', url:sourceCodeUrl]]])
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch', localBranch: 'master'],[$class: 'WipeWorkspace'], [$class: 'UserExclusion', excludedUsers: 'amguist']], gitTool: 'Git 1.9.4', submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github_user', url:sourceCodeUrl]]])
     } else {
         println("Current value of source code url: " + sourceCodeUrl)
     }

5f7fce1e7b737c4cce31886423dd8d0eb83b35ab
Aaron Guist <rhon.guist@gmail.com>
2023-08-16 10:40:05 -0700

    Adding initial file for deployment

--- devOpsWorkFlow.groovy
+++ devOpsWorkFlow.groovy
@@ -5,7 +5,7 @@ def sourceCodeCheckout(sourceCodeUrl) {
             $class: 'GitSCM',
             branches: [[name: '*/main']],
             doGenerateSubmoduleConfigurations: false,
-            extensions: [[$class: 'CleanCheckout']],
+            extensions: [[[$class: 'WipeWorkspace']], [[$class: 'CleanCheckout']]],
             submoduleCfg: [],
             userRemoteConfigs: [[credentialsId: 'githubToken', url:sourceCodeUrl]]
         ])
